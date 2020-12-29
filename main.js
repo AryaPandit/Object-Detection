8e77e3f0802ef7a8a16a8f9cb397cc996fc7a2c8
AryaPandit <73287114+AryaPandit@users.noreply.github.com>
2020-12-29 15:52:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img = "";
 status = "";
+objects = [];
 
 function preload() {
   img = loadImage('dog_cat.jpg')
@@ -14,18 +15,22 @@ function setup() {
 
 function draw() {
     image(img, 0, 0, 640, 420);
-    fill("#F5115F")
-    text("Dog", 45, 75);
-    noFill();
-    stroke("#FFD700")
-    rect(30, 60, 450, 350);
+  
+        if(status != "")
+        {
+          for (1 = 0; 1 < objects.length; i++)
+          {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
 
-    fill("#F5115F")
-    text("Cat", 320, 120);
-    noFill();
-    stroke("#FFD700");
-    rect(300, 90, 270, 320);
-}
+            fill("#F90261");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#000000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+          }
+        }
+      }        
 
 function modelLoaded() {
   console.log("Model Loaded!")
@@ -38,4 +43,5 @@ function gotResult(error, results) {
     console.log(error);
   }
   console.log(results);
+  objects = results;
 }
