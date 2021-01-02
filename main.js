b17cc86970605b64265b6a9d316d6284a0102d23
AryaPandit <73287114+AryaPandit@users.noreply.github.com>
2021-01-02 14:48:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,10 +9,10 @@ function preload() {
 function setup() {
     canvas = createCanvas(380, 380);
     canvas.center();
-    video = createCapture(VIDEO)
+    video = createCapture(VIDEO);
     video.size(380,380);
     video.hide();
-    objectDetector = ml5.objectDetector('cocossd', modelLoaded)
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 
